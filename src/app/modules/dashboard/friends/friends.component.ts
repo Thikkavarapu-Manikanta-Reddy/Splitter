62cdb3034ad2852d346bbd7a220961577d993ebb
THIKKAVARAPU MANIKANTA REDDY <t.manikantareddy160@gmail.com>
2021-06-17 20:46:53 +0530

    Changes

--- src/app/modules/dashboard/friends/friends.component.ts
+++ src/app/modules/dashboard/friends/friends.component.ts
@@ -41,35 +41,37 @@ export class FriendsComponent implements OnInit {
     }
     else {
 
+      let friendsData = {
+        "name": this.FriendForm.value.userName,
+        "email": this.FriendForm.value.email,
+        "amount": 0,
+        "percentage": 0
+      };
+
+      if (this.friendsList == null) {
+        this.friendsList = {};
+        this.friendsList[this.userData.email] = [];
+      }
+      if (this.friendsList[this.userData.email] == undefined) {
+        this.friendsList[this.userData.email] = [];
+      }
+
       let proceed = 1;
-      for(let i=0;i<this.friendsList[this.userData.email].length;i++) {
-        if(this.FriendForm.value.email == this.friendsList[this.userData.email][i].email) {
+
+      for (let i = 0; i < this.friendsList[this.userData.email].length; i++) {
+        if (this.FriendForm.value.email == this.friendsList[this.userData.email][i].email) {
           proceed = 0;
           break;
         }
       }
 
-      if(proceed == 1) {
-        let friendsData = {
-          "name": this.FriendForm.value.userName,
-          "email": this.FriendForm.value.email,
-          "amount": 0,
-          "percentage": 0
-        };
-  
-        if (this.friendsList == null) {
-          this.friendsList = {};
-          this.friendsList[this.userData.email] = [];
-        }
-        if (this.friendsList[this.userData.email] == undefined) {
-          this.friendsList[this.userData.email] = [];
-        }
+      if (proceed == 1) {
         this.friendsList[this.userData.email].push(friendsData);
-  
+
         this.storageService.setFriendsDetails(this.friendsList);
         // this.friendsList = this.storageService.getFriendsDetails();
         this.loggedFriendsList = this.friendsList[this.userData.email];
-  
+
         this.FriendForm.patchValue({
           userName: null,
           email: null
@@ -78,6 +80,7 @@ export class FriendsComponent implements OnInit {
       else {
         this.toasterService.showError("Already a friend with the same emailId is added to your friends list !!");
       }
+
     }
 
   }
